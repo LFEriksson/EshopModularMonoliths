43830cf0f763c03f6007ed5bb0cdb0e59c05d3e8
LFEriksson <Filip_Eriksson@live.se>
2024-11-29 11:07:52 +0100

    Product seeder

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Shared.Data;
 
 namespace Catalog;
 
@@ -19,6 +18,8 @@ public static class CatalogModule
         var connectionString = configuration.GetConnectionString("DefaultConnection");
         services.AddDbContext<CatalogDbContext>(options =>
             options.UseNpgsql(connectionString));
+
+        services.AddScoped<IDataSeeder, CatalogDataSeeder>();
 
         return services;
     }
---
+++ src/Modules/Catalog/Catalog/Data/Seed/InitialData.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Data.Seed;
+
+public static class InitialData
+{
+    public static List<Product> Products => new List<Product>
+    {
+        Product.Create(Guid.NewGuid(), "Product1", new List<string> { "category1" }, "Description for Product 1", "imagefile1", 100),
+        Product.Create(Guid.NewGuid(), "Product2", new List<string> { "category1" }, "Description for Product 2", "imagefile2", 200),
+        Product.Create(Guid.NewGuid(), "Product3", new List<string> { "category2" }, "Description for Product 3", "imagefile3", 300),
+        Product.Create(Guid.NewGuid(), "Product4", new List<string> { "category2" }, "Description for Product 4", "imagefile4", 400),
+        Product.Create(Guid.NewGuid(), "Product5", new List<string> { "category2" }, "Description for Product 5", "imagefile5", 500)
+    };
+}
