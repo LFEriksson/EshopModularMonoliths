fe7db62eb7b09f11ea1e135cb5e04497f1326e7b
LFEriksson <Filip_Eriksson@live.se>
2024-11-29 22:40:56 +0100

    skapat CQRS interfaces

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductCommand
+    (string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Shared/Shared/CQRS/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Shared.CQRS;
+
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+public interface ICommandHandler<in TCommand, TRespose> : IRequestHandler<TCommand, TRespose>
+    where TCommand : ICommand<TRespose>
+    where TRespose : notnull
+{
+}
