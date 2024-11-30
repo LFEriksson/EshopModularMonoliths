ecfbb71a9e160a8a328126303f78232ab9d2b2d3
LFEriksson <Filip_Eriksson@live.se>
2024-11-30 23:38:12 +0100

    CreateProductCommandValidator

--- src/Modules/Catalog/Catalog/GlobalUsing.cs
+++ src/Modules/Catalog/Catalog/GlobalUsing.cs
@@ -17,3 +17,5 @@ global using Carter;
 global using Microsoft.AspNetCore.Routing;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Http;
+global using FluentValidation;
+
