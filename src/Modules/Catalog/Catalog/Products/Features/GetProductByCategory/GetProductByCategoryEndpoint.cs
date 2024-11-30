01d9ed2ab66111631a486b552e9b8d30f6fa8d76
LFEriksson <Filip_Eriksson@live.se>
2024-11-30 17:31:50 +0100

    GetProductByCategoryEndpoint

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.Products.Features.GetProductByCategory;
+
+public record GetProductByCategoryResponse(IEnumerable<ProductDto> Products);
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+            var query = new GetProductByCategoryQuery(category);
+            var result = await sender.Send(query);
+            var response = result.Adapt<GetProductByCategoryResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Product By Category")
+        .WithDescription("Get Product By Category");
+    }
+}
