db8b605fc7cb0def3a4863901cdbb050356cc8a1
LFEriksson <Filip_Eriksson@live.se>
2024-12-01 16:52:56 +0100

    validators för update och delete

--- src/Modules/Catalog/Catalog/Products/Features/DeleteProduct/DeleteProductHandler.cs
+++ src/Modules/Catalog/Catalog/Products/Features/DeleteProduct/DeleteProductHandler.cs
@@ -4,6 +4,14 @@ public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 
 public record DeleteProductResult(bool IsSuccess);
 
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+    }
+}
+
 public class DeleteProductHandler(CatalogDbContext dbContext) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
     public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
