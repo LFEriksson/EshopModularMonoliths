53bed7a7d3891668b8d323a4674924295b6195fb
LFEriksson <Filip_Eriksson@live.se>
2024-12-20 00:46:44 +0100

    CreateBasketEndpoint

---
+++ src/Modules/Basket/Basket/Features/CreateBasket/CreateBasketEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.Basket.Features.CreateBasket;
+
+public record CreateBasketRequeest(ShoppingCartDto ShoppingCartDto);
+
+public record CreateBasketRespose(Guid Id);
+
+public class CreateBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (CreateBasketRequeest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateBasketCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateBasketRespose>();
+            return Results.Created($"/basket/{response.Id}", response);
+        })
+        .Produces<CreateBasketRespose>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Basket")
+        .WithDescription("Create Basket");
+    }
+}
