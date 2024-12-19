b8661d209073a72a1971a3016b99f14ac48bc079
LFEriksson <Filip_Eriksson@live.se>
2024-12-19 17:37:36 +0100

    AddItemIntoBasketHandler

---
+++ src/Modules/Basket/Basket/Features/AddItemIntoBasket/AddItemIntoBasketHandler.cs
@@ -0,0 +1,43 @@
+﻿namespace Basket.Basket.Features.AddItemIntoBasket;
+
+public record AddItemIntoBasketCommand(string UserName, ShoppingCartItemDto ShoppingCartItemDto)
+    : ICommand<AddItemIntoBasketResult>;
+
+public record AddItemIntoBasketResult(Guid id);
+
+public class AddItemIntoBasketCommandValidator : AbstractValidator<AddItemIntoBasketCommand>
+{
+    public AddItemIntoBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        RuleFor(x => x.ShoppingCartItemDto.ProductId).NotEmpty().WithMessage("ProductId is required");
+        RuleFor(x => x.ShoppingCartItemDto.Quantity).GreaterThan(0).WithMessage("Quantity should be greater than 0");
+    }
+}
+
+
+internal class AddItemIntoBasketHandler(BasketDbContext dbContext) : ICommandHandler<AddItemIntoBasketCommand, AddItemIntoBasketResult>
+{
+    public async Task<AddItemIntoBasketResult> Handle(AddItemIntoBasketCommand command, CancellationToken cancellationToken)
+    {
+        var shoppingCart = await dbContext.ShoppingCarts
+            .Include(x => x.Items)
+            .SingleOrDefaultAsync(x => x.UserName == command.UserName, cancellationToken);
+
+        if (shoppingCart is null)
+        {
+            throw new BasketNotFoundException(command.UserName);
+        }
+
+        shoppingCart.AddItem(
+            command.ShoppingCartItemDto.ProductId,
+            command.ShoppingCartItemDto.Quantity,
+            command.ShoppingCartItemDto.Price,
+            command.ShoppingCartItemDto.Color,
+            command.ShoppingCartItemDto.ProductName);
+
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new AddItemIntoBasketResult(shoppingCart.Id);
+    }
+}
