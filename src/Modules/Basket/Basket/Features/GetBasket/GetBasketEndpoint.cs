878aaee173eab02bbac9403f683cb708b72ab39b
LFEriksson <Filip_Eriksson@live.se>
2024-12-20 01:36:26 +0100

    GetBasketEndpoint

---
+++ src/Modules/Basket/Basket/Features/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.Basket.Features.GetBasket;
+
+public record GetBasketResponse(ShoppingCartDto ShoppingCart);
+public class GetBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/Basket/{username}", async (string userName, ISender sender) =>
+        {
+            var response = await sender.Send(new GetBasketQuery(userName));
+            return response.ShoppingCart;
+        })
+        .Produces<ShoppingCartDto>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Basket")
+        .WithDescription("Get Basket");
+    }
+}
