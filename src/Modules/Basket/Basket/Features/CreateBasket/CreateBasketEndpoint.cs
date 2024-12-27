07748e732d2e949795fad950676fe1b22e744337
LFEriksson <96394936+LFEriksson@users.noreply.github.com>
2024-12-27 15:13:00 +0100

    auth test

--- src/Modules/Basket/Basket/Features/CreateBasket/CreateBasketEndpoint.cs
+++ src/Modules/Basket/Basket/Features/CreateBasket/CreateBasketEndpoint.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.Basket.Features.CreateBasket;
+﻿using System.Security.Claims;
+
+namespace Basket.Basket.Features.CreateBasket;
 
 public record CreateBasketRequeest(ShoppingCartDto ShoppingCartDto);
 
@@ -8,8 +10,17 @@ public class CreateBasketEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/basket", async (CreateBasketRequeest request, ISender sender) =>
+        app.MapPost("/basket", async (
+            CreateBasketRequeest request, 
+            ISender sender
+            //,ClaimsPrincipal user
+            ) =>
         {
+            //var userName = user.Identity!.Name;
+            //var updateShoppingCart = request.ShoppingCartDto with { UserName = userName! };
+
+            //var command = new CreateBasketCommand(updateShoppingCart);
+
             var command = request.Adapt<CreateBasketCommand>();
             var result = await sender.Send(command);
             var response = result.Adapt<CreateBasketRespose>();
@@ -19,6 +30,7 @@ public class CreateBasketEndpoint : ICarterModule
         .ProducesProblem(StatusCodes.Status400BadRequest)
         .WithSummary("Create Basket")
         .WithDescription("Create Basket")
-        .RequireAuthorization();
+        //.RequireAuthorization()
+        ;
     }
 }
