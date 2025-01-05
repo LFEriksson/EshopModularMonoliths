75f5811e16884ba5b5591c88693770f9c7aa6cfe
LFEriksson <Filip_Eriksson@live.se>
2025-01-05 22:05:12 +0100

    BasketCheckoutIntegrationEventHandler

---
+++ src/Modules/Ordering/Orders/EventHandlers/BasketCheckoutIntegrationEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using Ordering.Orders.Features.CreateOrder;
+using Shared.Messaging.Events;
+
+namespace Ordering.Orders.EventHandlers;
+
+public class BasketCheckoutIntegrationEventHandler(
+    ISender sender,
+    ILogger<BasketCheckoutIntegrationEventHandler> logger) : IConsumer<BasketCheckoutIntegrationEvent>
+{
+    public async Task Consume(ConsumeContext<BasketCheckoutIntegrationEvent> context)
+    {
+        logger.LogInformation("Integration Event handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+        var createOrderCommand = MapToCreateOrderCommand(context.Message);
+        await sender.Send(createOrderCommand);
+    }
+
+    private CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutIntegrationEvent message)
+    {
+        var addressDto = new AddressDto(message.FirstName, message.LastName, message.EmailAddress, message.AddressLine, message.Country, message.State, message.ZipCode);
+        var paymentDto = new PaymentDto(message.CardName, message.CardNumber, message.Expiration, message.Cvv, message.PaymentMethod);
+        var orderId = Guid.NewGuid();
+
+        var orderDto = new OrderDto(
+            Id: orderId,
+            CustomerId: message.CustomerId,
+            OrderName: message.UserName,
+            ShippingAddress: addressDto,
+            BillingAddress: addressDto,
+            Payment: paymentDto,
+            Items: new List<OrderItemDto>() //TODO: Add items
+            );
+
+        return new CreateOrderCommand(orderDto);
+    }
+}
