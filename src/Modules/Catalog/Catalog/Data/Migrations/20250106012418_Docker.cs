0428b2e30c449a60f980055259e12fe50f05f3bb
LFEriksson <Filip_Eriksson@live.se>
2025-01-06 02:36:23 +0100

    docker fix

---
+++ src/Modules/Catalog/Catalog/Data/Migrations/20250106012418_Docker.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Docker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                schema: "catalog",
+                table: "products",
+                newName: "LastModifiedUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                schema: "catalog",
+                table: "products",
+                newName: "CreatedAtUTC");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedUTC",
+                schema: "catalog",
+                table: "products",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAtUTC",
+                schema: "catalog",
+                table: "products",
+                newName: "CreatedAt");
+        }
+    }
+}
