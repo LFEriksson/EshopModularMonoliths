fcfbe88b3a575c2e1c2e4431b97b5f0cda0b9ef8
LFEriksson <Filip_Eriksson@live.se>
2025-01-06 03:37:50 +0100

    docker identity

---
+++ src/Modules/Basket/Basket/Data/Migrations/20250106021546_Docker.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Basket.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Docker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                schema: "basket",
+                table: "ShoppingCarts",
+                newName: "LastModifiedUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                schema: "basket",
+                table: "ShoppingCarts",
+                newName: "CreatedAtUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                schema: "basket",
+                table: "ShoppingCartItems",
+                newName: "LastModifiedUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                schema: "basket",
+                table: "ShoppingCartItems",
+                newName: "CreatedAtUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                schema: "basket",
+                table: "OutboxMessages",
+                newName: "LastModifiedUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                schema: "basket",
+                table: "OutboxMessages",
+                newName: "CreatedAtUTC");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedUTC",
+                schema: "basket",
+                table: "ShoppingCarts",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAtUTC",
+                schema: "basket",
+                table: "ShoppingCarts",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedUTC",
+                schema: "basket",
+                table: "ShoppingCartItems",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAtUTC",
+                schema: "basket",
+                table: "ShoppingCartItems",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedUTC",
+                schema: "basket",
+                table: "OutboxMessages",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAtUTC",
+                schema: "basket",
+                table: "OutboxMessages",
+                newName: "CreatedAt");
+        }
+    }
+}
---
+++ src/Modules/Ordering/Data/Migrations/20250106021633_Docker.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Docker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                schema: "ordering",
+                table: "Orders",
+                newName: "LastModifiedUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                schema: "ordering",
+                table: "Orders",
+                newName: "CreatedAtUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModified",
+                schema: "ordering",
+                table: "OrderItems",
+                newName: "LastModifiedUTC");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                schema: "ordering",
+                table: "OrderItems",
+                newName: "CreatedAtUTC");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedUTC",
+                schema: "ordering",
+                table: "Orders",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAtUTC",
+                schema: "ordering",
+                table: "Orders",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "LastModifiedUTC",
+                schema: "ordering",
+                table: "OrderItems",
+                newName: "LastModified");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAtUTC",
+                schema: "ordering",
+                table: "OrderItems",
+                newName: "CreatedAt");
+        }
+    }
+}
